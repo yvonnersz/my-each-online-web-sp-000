33acba36a97a979e69c2984fcf70624dff210ace
Yvonne M Pham <yvonnersz@gmail.com>
2020-03-27 15:47:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,15 @@
 def my_each(array) # put argument(s) here
   # code here
+<<<<<<< HEAD
     i=0
     while i<array.length
       yield (array[i])
       i+=1
     end
     array
+=======
+  my_each(array) do |element|
+    puts element
+  end
+>>>>>>> 5ddac10e66a7c45c9fd3b97c17961c7914ece7ab
 end
